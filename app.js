97cc6f7357d6d97ca00e56755fd109e551893bad
PrateekMDesai04 <prateekmdesai04@gmail.com>
2021-04-17 19:19:40 +0400

    DB Setup
    Established connection using MongoURI

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express');
+const express = require('express'); //import package
+const app = express(); //execute package
+const mongoose = require('mongoose');
+require('dotenv/config'); // to get .env variables
 
-const app = express();
+// Middleware
+app.use('/posts', () => {
+  console.log('Middleware Runs');
+});
 
 // Create Routes
 app.get('/', (req, res) => {
@@ -8,6 +14,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/posts', (req, res) => res.send('Posts'));
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  () => console.log('Connected to DB !')
+);
 
 // Start listening to the server
 app.listen(4200, () => console.log('Server Up and Running...'));
